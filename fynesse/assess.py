177dc01308365737028600e1d29286726f2cce38
atreyi13 <51878098+atreyi13@users.noreply.github.com>
2022-12-01 10:17:39 +0000

    Update assess.py

--- fynesse/assess.py
+++ fynesse/assess.py
@@ -140,8 +140,8 @@ def prices_over_time(df): #This returns a graph of the prices over time of diffe
 
     x = sorted(list(set((pd.DatetimeIndex( df['date_of_transfer']).to_period('M')).strftime('%Y%m'))))
     x3 = sorted(list(set((pd.DatetimeIndex( df[df['property_type'] == 'O']['date_of_transfer']).to_period('M')).strftime('%Y%m'))))
-    #newticks = [j for i,j in enumerate(x) if not i%24]
-    newticks = x
+    newticks = [j for i,j in enumerate(x) if not i%24]
+    #newticks = x
 
     fig = plt.figure(figsize=(15,15))
     (ax1, ax2, ax3) = fig.subplots(3, 2)
@@ -217,8 +217,8 @@ def log_price_over_time(df): #This is the same as the previous view, except wear
 
     x = sorted(list(set((pd.DatetimeIndex( df['date_of_transfer']).to_period('M')).strftime('%Y%m'))))
     x3 = sorted(list(set((pd.DatetimeIndex( df[df['property_type'] == 'O']['date_of_transfer']).to_period('M')).strftime('%Y%m'))))
-    #newticks = [j for i,j in enumerate(x) if not i%24]
-    newticks = x
+    newticks = [j for i,j in enumerate(x) if not i%24]
+    #newticks = x
 
     fig = plt.figure(figsize=(15,15))
     (ax1, ax2, ax3) = fig.subplots(3, 2)
